39111379444fcc3d81d2f22af65ac511cb371279
Saint George Aufranc <stgaufranc@gmail.com>
2026-01-28 06:19:42 -0800

    MySQL initializer file, start of docker-compose and dockerfile

---
+++ backend/db/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Schedules (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sched_name VARCHAR(255) NOT NULL,
+    owner_id INT,
+    FOREIGN KEY(owner_id) REFERENCES Users(id)
+);
+
+CREATE TABLE IF NOT EXISTS Assignments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    assignment_name VARCHAR(255) NOT NULL,
+    work_load INT,
+    notes VARCHAR(2048),
+    schedule_id INT,
+    FOREIGN KEY(schedule_id) REFERENCES Schedules(id)
+);
