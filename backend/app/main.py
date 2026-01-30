aafc9733f5f33861f33b7eb863eb9f849d42ca8f
Saint George Aufranc <stgaufranc@gmail.com>
2026-01-30 05:39:09 -0800

    add outline of main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,24 @@
 # Flask entry point.
 # TODO: Create Flask app, load config (core.config), register blueprints for api.auth,
 #       api.syllabus, api.schedule, api.export. Run with FLASK_APP=app.main.
+
+from flask import Flask
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host = "mysql",
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        database = os.getenv("DB_NAME")
+    )
+
+@app.route("/")
+def index():
+    return"sample index text"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
